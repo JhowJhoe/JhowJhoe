c02e34bc95e3a2cd31fbca41bec8faa5b6c0b27c
jhowjhoe <jhowjhoe@gmail.com>
2017-05-29 09:51:08 -0300

    Alterações
    Primeira modificação

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title> JhowJhoe - Developer </title>
+
+  <!-- Path CSS -->
+      <link type="text/css" rel="stylesheet" href="css/bootstrap.css "/>
+      <link type="text/css" rel="stylesheet" href="css/style.css" />
+      <link type="text/css" rel="stylesheet" href="css/font-awesome.css" />
+      <!--[if lt IE 9]>
+      <script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <![endif]-->
+  <!-- Fontes Google -->
+      <!-- Google fonts - witch you want to use - (rest you can just remove) -->
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+  <!-- Biblioteca Jquery versão 3.2.0 -->
+      <script type="text/javascript" src="js/jquery-3.2.0.js"></script>
+      <script type="text/javascript" src="js/bootstrap.js"></script>
+  <!-- Icone mobile -->
+      <link rel="apple-touch-icon" href="imagens/logo.png">
+  </head>
+
+  <body>
+  <div class="container">
+    Teste
+    </div> 
